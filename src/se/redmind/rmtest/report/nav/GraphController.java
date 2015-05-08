3d65c772563924094d1a8676ade5b040af6f3e00
MatteK <mattek@outlook.com>
2015-05-08 12:04:28 +0200

    Added clickLegend and isEnabled function

--- src/se/redmind/rmtest/report/nav/GraphController.java
+++ src/se/redmind/rmtest/report/nav/GraphController.java
@@ -44,6 +44,38 @@ public class GraphController extends BaseController{
 		return list.get(index);
 	}
 	
+	public void clickLegend(int index){
+		String legendCSS = "g[class^='highcharts-legend']> g:nth-of-type(1) > g:nth-of-type(1) > g:nth-of-type(" + index + ")";
+		driverFluentWait(15).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(legendCSS)));
+		WebElement legend = driver.findElement(By.cssSelector(legendCSS));
+		legend.click();
+	}
+	
+	public boolean isEnabled(String type){
+		int indexSelector = 0;
+		switch (type) {
+		case "passed":
+			indexSelector = 1;
+			break;
+		case "skipped":
+			indexSelector = 2;
+			break;
+		case "failed":
+			indexSelector = 3;
+			break;
+		default:
+			break;
+		}
+		String passedCSS = "g[class^='highcharts-legend']> g:nth-of-type(1) > g:nth-of-type(1) > g:nth-of-type(" + indexSelector + ") > rect:nth-of-type(1)";
+		driverFluentWait(15).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(passedCSS)));
+		WebElement passed = driver.findElement(By.cssSelector(passedCSS));
+		String fill = passed.getAttribute("fill");
+		if(fill.equals("#CCC"))
+			return false;
+		else
+			return true;
+	}
+	
 	public void clickDownArrow(){
 		String downArrowCSS = "g[class^='highcharts-legend']> g:nth-of-type(2) >  path:nth-of-type(2)";
 		driverFluentWait(15).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(downArrowCSS)));
