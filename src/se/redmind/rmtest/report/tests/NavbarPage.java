342cf6733b35c960815253d551fdea123cbaabec
MatteK <mattek@outlook.com>
2015-05-11 15:30:21 +0200

    Fix?

--- src/se/redmind/rmtest/report/tests/NavbarPage.java
+++ src/se/redmind/rmtest/report/tests/NavbarPage.java
@@ -55,7 +55,7 @@ public class NavbarPage {
 	        for (int i = 0; i < wrapperList.length; i++) {
 	            returnList.add(new Object[]{wrapperList[i], wrapperList[i].toString()});
 	        }
-
+ 
 	        return returnList;
 	    }
 
@@ -134,15 +134,4 @@ public class NavbarPage {
 	    	nav.chooseTimeStamp(expected);
 	    	assertEquals(expected, actual);
 	    }
-	    
-	    
-	    	
-	    	
-	    
-
 }
-
-	    
-	    
-	    
-
