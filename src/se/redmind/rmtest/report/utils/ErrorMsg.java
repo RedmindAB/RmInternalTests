9fb227b0232d91bcf9ffa12387c2cc8c62d8b66d
Vintr <victor.lovgren@redmind.se>
2015-06-15 13:11:07 +0200

    Cleaned up and edited error messages

--- src/se/redmind/rmtest/report/utils/ErrorMsg.java
+++ src/se/redmind/rmtest/report/utils/ErrorMsg.java
@@ -2,9 +2,8 @@ package se.redmind.rmtest.report.utils;
 
 public class ErrorMsg {
 	
-	public static final String LegendListText = "\n Expected text in legend list does not match the actual text; \n";
-	public static final String LegendListTextFirst = "\n First expected text in legend list does not match the first actual text; \n";
-	public static final String LegendListTextSecond = "\n Second expected text in legend list does not match the second actual text; \n";
+	//assertEquals error message
+	public static final String LegendListTextIsDifferent = "\n Expected text in legend list does not match the actual text; \n See this assert: \n";
 	
 	public static final String LegendListSize = "\n Expected amount of rows in legend list does not match the actual amount of rows; \n";
 	
@@ -23,10 +22,6 @@ public class ErrorMsg {
 	public static final String ChartTitleIsDifferent = "\n Expected chart title differs from actual chart title. \n See this assert: ";
 	//assertNotEquals error message
 	public static final String ChartTitleIsSame = "\n Expected chart title does NOT differ from actual chart title. \n See this assert: ";
-	
-//	public static final String ChartTitleFirst = "\n First expected chart title differs from first actual chart title \n";
-//	public static final String ChartTitleSecond = "\n Second expected chart title differs from second actual chart title \n";
-//	public static final String ChartTitleThird = "\n Third expected chart title differs from third actual chart title \n";
 	
 	public static final String HighchartsTitle = "Expected text in the highcharts title differs from the actual text; \n";
 	public static final String HighchartsSubTitle = "\n Expected text in the highcharts (sub)title differs from the actual text; \n";
