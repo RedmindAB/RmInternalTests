136598b7ea3dbe4a167b642d7a170bebaac28d9d
Vintr <victor.lovgren@redmind.se>
2015-07-29 13:08:20 +0200

    Fixed failed tests try #3

--- src/se/redmind/rmtest/report/tests/VisualPage.java
+++ src/se/redmind/rmtest/report/tests/VisualPage.java
@@ -78,7 +78,7 @@ public class VisualPage {
 	    public void openSysos(){
 	    	nav.changeProject("RMReportSuite");
 			nav.chooseClass("se.redmind.rmtest.report.tests.GraphPage");
-			nav.openMethod("4");
+			nav.openMethod("13");
 	    	nav.openSysos();
 	    	assertTrue(ErrorMsg.SysoDidNotOpen + "1 \n", nav.isSysosOpen());
 	    }	    
@@ -144,7 +144,7 @@ public class VisualPage {
 		public void isThumbnailPresent(){
 			nav.changeProject("RMReportSuite");
 			nav.chooseClass("se.redmind.rmtest.report.tests.GraphPage");
-			nav.openMethod("4");
+			nav.openMethod("13");
 			assertTrue(ErrorMsg.ScreenshotThumbnailNotPresent + "1 \n", nav.isThumbnailPresent("0"));
 		}
 		
